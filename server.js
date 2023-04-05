ddcedf6e19784080acddad912b18d0ce9905b25f
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-04-05 19:11:47 -0400

    server.js added for routing

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const dbData = require("./db/db.json");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
